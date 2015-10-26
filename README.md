1fdf6b640d64f8317b004e17582b4bfbceadf6b4
Markus Kohlhase <mail@markus-kohlhase.de>
2015-10-26 04:57:24 +0100

    added docs

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # r2d2-cypher
 
-r2d2 connection pool for cypher based on
+[r2d2](https://github.com/sfackler/r2d2) connection pool for
+[cypher](https://en.wikipedia.org/wiki/Cypher_Query_Language) based on
 [rusted-cypher](https://github.com/livioribeiro/rusted-cypher).
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,49 @@
 // Copyright (c) 2015 Markus Kohlhase <mail@markus-kohlhase.de>
 
+#![deny(missing_docs,
+        missing_debug_implementations, missing_copy_implementations,
+        trivial_casts, trivial_numeric_casts,
+        unsafe_code,
+        unstable_features,
+        unused_import_braces, unused_qualifications)]
+
+//! [r2d2-cypher](https://github.com/flosse/r2d2-cypher) is a
+//! [r2d2](https://github.com/sfackler/r2d2) connection pool for
+//! [rusted-cypher](https://github.com/livioribeiro/rusted-cypher).
+//!
+//! # Example
+//!
+//! ```
+//! extern crate r2d2;
+//! extern crate r2d2_cypher;
+//!
+//! use r2d2::{Config, Pool};
+//! use r2d2_cypher::CypherConnectionManager;
+//!
+//! pub fn main() {
+//!   let db_url  = "http://neo4j:neo4j@127.0.0.1:7474/db/data".to_string();
+//!   let manager = CypherConnectionManager{url:db_url};
+//!   let config  = Config::builder().pool_size(5).build();
+//!   let pool    = Pool::new(config, manager).unwrap();
+//!   let client  = pool.clone().get().unwrap();
+//!   let result  = client.cypher().exec("MATCH (n)-[r]->() RETURN n");
+//! }
+//! ```
+
 extern crate r2d2;
 extern crate rusted_cypher;
 
 use rusted_cypher::GraphClient;
 use rusted_cypher::error::GraphError;
 
-pub struct CypherConnectionManager { pub url: String }
+/// A struct that holds connection specific information.
+#[derive(Debug)]
+pub struct CypherConnectionManager {
+  /// the URL to the database
+  pub url: String,
+}
 
-impl r2d2::ManageConnection for CypherConnectionManager{
+impl r2d2::ManageConnection for CypherConnectionManager {
   type Connection = GraphClient;
   type Error = GraphError;
 
@@ -23,7 +58,7 @@ impl r2d2::ManageConnection for CypherConnectionManager{
     }
   }
 
-  fn has_broken(&self, conn: &mut GraphClient) -> bool {
+  fn has_broken(&self, _: &mut GraphClient) -> bool {
     false
   }
 }
